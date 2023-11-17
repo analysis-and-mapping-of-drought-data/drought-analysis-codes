59e75160235c9a416b83838055cdb071e30f1b69
Mustafa Tortuk <mustafat237@hotmail.com>
2023-11-17 15:09:47 +0300

    deneme1

--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(() => console.log('Db connected'))
     .catch((err) => console.log(err));
 
+    //kk
+
 // Baraj rotalarını kullan
 app.use('/api', barajRoutes);
 
