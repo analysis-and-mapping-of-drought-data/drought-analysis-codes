9cc0d0d38de26acab2ad3241848923c1a81e1ece
Berat <kurtaranberat@gmail.com>
2023-11-17 13:32:54 +0300

    index dosyası eklendi

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const barajRoutes = require('../server/routes/BarajRoute');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const port =3001;
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB'ye bağlan
+mongoose.connect(process.env.MONGO_URL, {
+     useNewUrlParser: true, useUnifiedTopology: true
+    })
+    .then(() => console.log('Veri tabanına baglandı '))
+    .catch((err) => console.log(err));
+
+// Baraj rotalarını kullan
+app.use('/api', barajRoutes);
+
+// Server'ı dinle
+app.listen(port, () => {
+  console.log(`Server ${port} portunda calisiyore`);
+});
