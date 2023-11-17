408df2d0731b4ab314d8804b8088ee3232c82690
Mustafa Tortuk <mustafat237@hotmail.com>
2023-11-17 21:40:49 +0300

    deneme

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ const port =3001;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB'ye bağlan
+// MongoDB'ye bağlanma
 mongoose.connect(process.env.MONGO_URL, {
      useNewUrlParser: true, useUnifiedTopology: true
     })
-    .then(() => console.log('DB baglandi.'))
+    .then(() => console.log('DB baglanti.'))
     .catch((err) => console.log(err));
 
 // Baraj rotalarını kullan
