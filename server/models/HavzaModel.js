c1896ceb233ce2aff4da0c9e1ce7eeca08c39b79
emineGUNES <emine.lalegns@gmail.com>
2023-12-13 23:21:54 +0300

    havzalar model oluşturuldu

---
+++ server/models/HavzaModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const HavzaSchema= new mongoose.Schema({
+    havza_adi: {
+        type: String,
+        required: true,
+    },
+    havza_yagisAlani: {
+        type: Number,
+        required: true,
+    },
+    baraj_dolulukOrani:{
+        type :Number,
+        required :true,
+    },
+    yil:{
+        type :Number,
+        required :true,
+    },
+    
+}, { timestamps: true });
+const Havza = mongoose.model('Havza', HavzaSchema);
+
+module.exports = Havza;
