1140776d97f315067249d9087e626ff8a95c096e
Mustafa Tortuk <mustafat237@hotmail.com>
2023-12-13 23:36:10 +0300

    DataTable eklendi ve düzenlendi.

--- client/src/components/Baraj.jsx
+++ client/src/components/Baraj.jsx
@@ -1,80 +1,150 @@
 import axios from "axios";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { useTable , usePagination } from 'react-table';
 
 function Baraj() {
     
     const [data, setData] = useState([]);
+
+    const columns = useMemo(
+      () => [
+        {
+          Header:"#",
+          accessor:"index"
+        },
+        {
+          Header: "Baraj",
+          accessor: "baraj_adi",
+        },
+        {
+          Header: "2010",
+          accessor: "yil_2010",
+        },
+        {
+          Header: "2011",
+          accessor: "yil_2011",
+        },
+        {
+          Header: "2012",
+          accessor: "yil_2012",
+        },
+        {
+          Header: "2013",
+          accessor: "yil_2013",
+        },
+        {
+          Header: "2014",
+          accessor: "yil_2014",
+        },
+        {
+          Header: "2015",
+          accessor: "yil_2015",
+        },
+        {
+          Header: "2016",
+          accessor: "yil_2016",
+        },
+        {
+          Header: "2017",
+          accessor: "yil_2017",
+        },
+        {
+          Header: "2018",
+          accessor: "yil_2018",
+        },
+        {
+          Header: "2019",
+          accessor: "yil_2019",
+        },
+        {
+          Header: "2020",
+          accessor: "yil_2020",
+        },
+        {
+          Header: "2021",
+          accessor: "yil_2021",
+        },{
+          Header:"Detay",
+          accessor:"detay",
+          Cell:({row})=>(
+            <button className="btn btn-primary">Detay</button>
+          )
+        }
+      ],
+      []
+    );
+    
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      page,
+      prepareRow,
+      canPreviousPage,
+      canNextPage,
+      pageOptions,
+      pageCount,
+      gotoPage,
+      nextPage,
+      previousPage,
+      setPageSize,
+      state: { pageIndex, pageSize },
+    } = useTable(
+      {
+        columns, // Change 'column' to 'columns'
+        data,
+        initialState: { pageIndex: 0, pageSize: 10 },
+      },
+      usePagination
+    );
   
     useEffect(() => {
       axios.get('http://localhost:3001/baraj/year')
         .then(res => {
-          console.log(res);
-          setData(res.data); // res.data içinde doğrudan barajlar bulunuyor.
+          const indexedData = res.data.map((item, index) => ({ ...item, index: index + 1 }));
+          setData(indexedData); // res.data içinde doğrudan barajlar bulunuyor.
         })
         .catch(err => console.log(err));
     }, []);
-  
-    const handleDelete = (id) => {
-      axios.delete(`http://localhost:3001/api/barajlar/${id}`)
-        .then(res => {
-          console.log(res);
-          // Silme işlemi başarıyla tamamlandığında sayfayı yenileme
-          setData(prevData => prevData.filter(user => user._id !== id));
-        })
-        .catch(err => console.log(err))
-    }
 
   return (
     <div className="container-fluid pt-4 px-4">
       <div className="bg-secondary text-center rounded p-4">
         <div className="d-flex align-items-center justify-content-between mb-4">
           <h6 className="mb-0">Recent Barajs</h6>
-          <Link to="/create" className="btn btn-success btn-sm">Add +</Link>
         </div>
         <div className="table-responsive">
-          <table className="table text-start align-middle table-bordered table-hover mb-0">
+          <table {...getTableProps()} className="table text-start align-middle table-bordered table-hover mb-0">
             <thead>
-              <tr className="text-white">
-                <th scope="col">Baraj</th>
-                <th scope="col">2010</th>
-                <th scope="col">2011</th>
-                <th scope="col">2012</th>
-                <th scope="col">2013</th>
-                <th scope="col">2014</th>
-                <th scope="col">2015</th>
-                <th scope="col">2016</th>
-                <th scope="col">2017</th>
-                <th scope="col">2018</th>
-                <th scope="col">2019</th>
-                <th scope="col">2020</th>
-                <th scope="col">2021</th>
-              </tr>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((columns) => (
+                    <th {...columns.getHeaderProps()}>{columns.render("Header")}</th>
+                  ))}
+                </tr>
+              ))}
             </thead>
-            <tbody>
-            {data.map((user, index) => (
-              <tr key={index}>
-                <td>{user.baraj_adi}</td>
-                <td>{user.yil_2010}</td>
-                <td>{user.yil_2011}</td>
-                <td>{user.yil_2012}</td>
-                <td>{user.yil_2013}</td>
-                <td>{user.yil_2014}</td>
-                <td>{user.yil_2015}</td>
-                <td>{user.yil_2016}</td>
-                <td>{user.yil_2017}</td>
-                <td>{user.yil_2018}</td>
-                <td>{user.yil_2019}</td>
-                <td>{user.yil_2020}</td>
-                <td>{user.yil_2021}</td>
-                <td>
-                  <Link to={`/edit/${user._id}`} className="btn btn-sm btn-success me-2">Update</Link>
-                  <button onClick={() => handleDelete(user._id)} className="btn btn-sm btn-danger">Delete</button>
-                </td>
-              </tr>
-             ))} 
+            <tbody {...getTableBodyProps()}>
+              {page.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    ))}
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
+          <div className="mt-3">
+            <button className="btn btn-secondary" onClick={()=>previousPage()} disabled={!canPreviousPage}>Prev</button>
+            <button className="btn btn-secondary" onClick={()=>nextPage()} disabled={!canNextPage}>Next</button>
+          </div>
+          <div>
+              Page {pageIndex + 1} of {pageOptions.length}
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,14 +6,13 @@ function Footer(){
       <div>
       {/* Footer Start */}
       <div className="container-fluid pt-2 px-2 fixed-bottom" style={{ maxWidth: '80%' }}>
-        <div className="bg-secondary rounded-top p-2">
+        <div className="bg-secondary rounded-top">
           <div className="row">
             <div className="col-12 col-sm-6 text-center text-sm-start">
               © <a href="#">Baraj ve sulama verileri </a>, Bu sitenin bütün hakları saklıdır ... 
             </div>
             <div className="col-12 col-sm-6 text-center text-sm-end">
-              Tasarım: <a>Berat Kurtaran </a>
-              <br />ISS Ekibi
+              Tasarım: <a>Berat Kurtaran </a>ISS Ekibi
             </div>
           </div>
         </div>
