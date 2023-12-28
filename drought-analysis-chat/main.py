e67e936c7b7d314da8e615816923ea6156c7d5ea
Mustafa Tortuk <mustafat237@hotmail.com>
2023-12-29 01:16:38 +0300

    Python kodları eklendi.

---
+++ drought-analysis-chat/main.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+import openai
+import requests
+import pandas as pd
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+app = Flask(__name__)
+
+apiKey = "***************************"
+openai.api_key = apiKey
+
+data = []
+
+@app.route("/chat", methods=["GET"])
+def askChat():
+
+    data = request.args.get("question")
+
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-0613",
+        prompt=data,
+        max_tokens=50
+    )
+
+    return jsonify({response:response.choices[0].text})
+
+@app.route("/ml", methods=["GET"])
+def ml():
+    try:
+
+        baraj_adi = request.args.get("baraj_adi")
+
+        response = requests.get(f'http://localhost:3001/baraj/year/{baraj_adi}')
+
+        global data
+        data = response.json()
+
+        df = pd.DataFrame(data)
+
+        csv_data = df.to_csv(index=False)
+        file_exists = os.path.isfile(f'{baraj_adi}.csv')
+
+        if not file_exists:
+            with open(f'{baraj_adi}.csv', 'w') as file:
+                file.write(csv_data)
+
+        df = pd.read_csv(f'{baraj_adi}.csv', delimiter=',')
+
+        X = df.drop(['baraj_adi'], axis=1)
+        y = df['baraj_adi']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+        model = LinearRegression()
+
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        return jsonify({"r2 score": r2})
+
+    except requests.exceptions.RequestException as e:
+        # Handle request exceptions
+        return jsonify({"error": str(e)})
+
+    except Exception as e:
+        # Handle other exceptions
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(port=6007)
